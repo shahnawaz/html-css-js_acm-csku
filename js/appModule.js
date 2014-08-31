f36774ce9bd5bbf067b6f3ce9b85c626b4bf730c
Muhammad Faizan <muhammad.faizan01@live.com>
2014-08-31 16:45:53 +0500

    table support added. Now data is being added and removed through maintaining a table...

--- js/appModule.js
+++ js/appModule.js
@@ -7,6 +7,20 @@ var AppModule = {
     data:{},
 
     selectedItems:[],
+    allItems: {},
+    loadItems: function(){
+
+        // load item only if its not already loaded
+        if (!AppModule.allItems.hasOwnProperty("a1")){
+            var keys = Object.keys(AppModule.data);
+            keys.forEach(function(key,index){
+                AppModule.data[key].forEach(function(item,index){
+                    AppModule.allItems[item.id] = item;
+                })
+            });
+        }
+
+    },
 
     tabChange : function(id){
         // first remove the previous tab 'nav-active' class
@@ -56,10 +70,11 @@ var AppModule = {
 
     addOrRemoveItem : function(itemId){
         var itemClicked = document.getElementById(itemId);
-
+        var index;
         if(AppModule.selectedItems.indexOf(itemId)>=0){
             //remove
-            AppModule.selectedItems.splice(AppModule.selectedItems.indexOf(itemId),1);
+            index = AppModule.selectedItems.indexOf(itemId);
+            AppModule.selectedItems.splice(index,1);
             itemClicked.className = "img";
         }
         else{
@@ -67,26 +82,63 @@ var AppModule = {
             AppModule.selectedItems.push(itemId);
             itemClicked.className += " item-selected"
         }
-        this.updateCart("cart-items");
+        this.updateCart("cart-items",index);
         this.updatePrice("cart-price");
-        console.log(AppModule.selectedItems);
+        //console.log(AppModule.selectedItems);
     },
 
-    updateCart : function(itemCartId){
-        var selectedItemsHtml = "";
-        for(obj in AppModule.data){
-            AppModule.data[obj].forEach(function(item){
-                AppModule.selectedItems.forEach(function(id){
-                    if(item.id == id){
-                        selectedItemsHtml += "<li class='cartItems'>"+item.name+"</li>";
-                    }
-                })
-            })
+    insertAnItem: function(item,table){
+        var newrow = table.insertRow(table.rows.length);
+        newrow.insertCell(0).innerHTML = item.name;
+        newrow.insertCell(0).innerHTML = "Rs." ;
+        newrow.insertCell(0).innerHTML = item.price;
+    },
+
+    updateCart : function(itemCartId,index){
+        AppModule.loadItems();
+        //var selectedItemsHtml = "";
+        /*for(obj in AppModule.data){
+	     AppModule.data[obj].forEach(function(item){
+	     AppModule.selectedItems.forEach(function(id){
+	     if(item.id == id){
+	     selectedItemsHtml += "<li class='cartItems'>"+item.name+ "<span style='float:right'>- Rs ";
+	     switch(true){
+	     case (item.price < 10):
+	     selectedItemsHtml += " &nbsp &nbsp";
+	     break;
+
+	     case (item.price < 100):
+	     selectedItemsHtml += " &nbsp";
+	     break;
+	     }
+
+	     selectedItemsHtml += item.price + "</span></li>";
+	     }
+	     })
+	     })
+	     }*/
+
+        var table = document.getElementById("cart-items-table");
+        if (index == undefined){
+            var selectedItem = AppModule.allItems[AppModule.selectedItems[AppModule.selectedItems.length-1]];
+            console.log(selectedItem);
+            var newRow = table.insertRow(table.rows.length);
+            newRow.width = "100%";
+            newRow.setAttribute("data-item",selectedItem.id);
+            var data = [selectedItem.name,"Rs",selectedItem.price];
+            for( var cell= 0 ; cell <3; cell++){
+                var tempCell = newRow.insertCell(cell);
+                tempCell.innerHTML = data[cell];
+                if(cell === 0){
+                    tempCell.className += " name-column"
+                } else  if (cell === 2){
+                    tempCell.className += " price-column"
+                }
+
+            }
+        } else {
+            table.deleteRow(index);
         }
-        var cart = document.getElementById(itemCartId);
-
-        cart.innerHTML = selectedItemsHtml;
-
     },
 
     updatePrice : function(priceCartId){
